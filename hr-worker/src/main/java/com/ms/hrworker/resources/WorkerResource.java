59cd0c2b17171f4e1c41dcba1210508f8eead3e9
Mateus Ferreira <mateusf63@gmail.com>
2024-04-23 12:42:32 -0300

    Update | db config server

--- hr-worker/src/main/java/com/ms/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/ms/hrworker/resources/WorkerResource.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -25,9 +24,6 @@ public class WorkerResource {
 	//OBS: Essa estrutura é uma alternativa apenas para verificar qual porta o projeto está rodando
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class); //Imprime mensagens de LOG no console
 	
-	@Value("${test.config}")
-	private String testConfig;
-	
 	@Autowired
 	private Environment env; //Contém informações do contexto da apllicação, como variaveis de configuração e etc
 	
@@ -42,20 +38,11 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs(){
-		logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable("id")Long id){
-		
-		/* TESTANDO TIMEOUT DE RESPOSTA*/
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 		logger.info("EXECUTED PORT = " + env.getProperty("local.server.port")); //Imprime no log a porta de execução deste projeto
 		
